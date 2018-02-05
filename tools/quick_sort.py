861cf71b8956d2784ac3006ae5797bbca2d45e23
ShichaoMa <270515529@qq.com>
2018-02-05 18:00:21 +0800

    add quick_sort script

---
+++ tools/quick_sort.py
@@ -0,0 +1,43 @@
+def quick(arr, start, end):
+    """
+    快排知识点总结
+    :param arr:
+    :param start:
+    :param end:
+    :return:
+    """
+    # 1 基准情形，两个指针相遇，开头不能大于等于结尾
+    if start >= end:
+        return
+    i = start
+    j = end
+    # 2 使用一个变量保存中间值，使用一个bool值来决定是从后向前扫描还是相反。
+    target = arr[i]
+    from_end = True
+    while i < j:
+        if from_end:
+            # 3 从后向前扫描，当中间值大于后值时交换
+            if target <= arr[j]:
+                j -= 1
+            else:
+                arr[i], arr[j] = arr[j], arr[i]
+                i += 1
+                from_end = False
+        else:
+            # 3 从前向后扫描，当中间值小于等于前值时交换
+            # 第3条必须保存中间值等于目标值时的情况被覆盖到
+            if target > arr[i]:
+                i += 1
+            else:
+                arr[i], arr[j] = arr[j], arr[i]
+                j -= 1
+                from_end = True
+    # 4 将数组从两个指针相遇的地方将数组分成两部分。
+    quick(arr, start, i)
+    quick(arr, i+1, end)
+
+
+if __name__ == "__main__":
+    arr = [6,3,8,1,4,6,9,2]
+    quick(arr, 0, len(arr)-1)
+    print(arr)
