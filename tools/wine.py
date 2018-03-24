8843438d1b4ce5c08b8408ce13fc77e4b9677fcb
ShichaoMa <270515529@qq.com>
2018-03-24 18:37:44 +0800

    Commit

--- tools/wine.py
+++ tools/wine.py
@@ -1,49 +1,3 @@
-# -*- coding:utf-8 -*-
-import json
-import time
-import requests
-
-import asyncio
-def crawl():
-
-    resp = requests.post("http://0.0.0.0:9999/api/discover",
-                         data={"data": json.dumps({"crawlid": "roc_%s"%time.strftime("%Y%m%d%H%M%S"),
-                         "source_site_code": "FFCHCN", "full": True,
-                         "url": "https://www.farfetch.cn/cn/shopping/men/buckled-shoes-2/items.aspx",
-                         "meta": {"tags": {"source_site_code": "FFCHCN", "tags": [], "brand_id": 1,
-                                           "gender_id": 1, "source_site_id": 1, "taxon_id": 3}}})})
-
-    print(resp.text)
-# class ApiTest(unittest.TestCase):
-#
-#     def test_feed(self):
-#         self.assertEqual()
-
-def tags():
-
-    resp = requests.post("http://192.168.200.31:9999/api/enrich/tags",
-                         data={"data": json.dumps({"source_site_code": "AMZN", "type": "ENRICH",
-                         "url": "https://www.amazon.com/s/ref=lp_679337011_ex_n_10?rh=n%3A7141123011%2Cn%3A7147440011%2Cn%3A679337011%2Cn%3A6127767011&bbn=679337011&ie=UTF8",
-                         "tags": ["aaa"]})})
-
-    print(resp.text)
-
-
-def update():
-    resp = requests.post("http://0.0.0.0:9999/api/update",
-                         json={
-                         "site": "FFCHCN",
-                         "full": True, "messages": [{"tags": [], "item_id": "", "meta": {"url": "https://www.farfetch.cn/cn/shopping/men/asics---item-12527082.aspx?storeid=9671&from=listing"}}]
-                         })
-
-    print(resp.text)
-
-
-
-#
-# update()
-
-
 from fractions import Fraction
 from toolkit import cache_property
 from functools import reduce
@@ -60,6 +14,7 @@ class Cup(object):
     求：倒入count杯酒时，第line行第col例杯中的酒量。
     """
     _total = set()
+
     def __init__(self, left_parent, right_parent, capacity):
         self.left_parent = left_parent
         self.right_parent = right_parent
@@ -90,13 +45,13 @@ class Cup(object):
         else:
             return None
 
-    def increase(self, count):
+    def pour(self, count):
         self._total.add(self)
         remainder = self.capacity + count - 1
         if remainder > 0:
             self.capacity = Fraction(1, 1)
-            self.left_child.increase(remainder/2)
-            self.right_child.increase(remainder/2)
+            self.left_child.pour(remainder/2)
+            self.right_child.pour(remainder/2)
         else:
             self.capacity += count
 
@@ -118,7 +73,7 @@ if __name__ == "__main__":
     # 创建一个酒杯(塔)
     cup = Cup(None, None, Fraction(0, 1))
     # 倒入105杯酒
-    cup.increase(105)
+    cup.pour(105)
     # 求第17行，第6列中的酒量
     print(cup.find(17, 6).capacity)
     print(cup.total())
