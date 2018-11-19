599b554cd63b9c4012e2d37c5cd381290249e432
马式超 <shichao.ma@yiducloud.cn>
2018-11-19 16:40:19 +0800

    Bugfix: 增加future包依赖

--- toolkit/__init__.py
+++ toolkit/__init__.py
@@ -17,7 +17,7 @@ from queue import Empty
 from future.utils import raise_from
 from functools import wraps, reduce, partial
 
-__version__ = '1.7.21'
+__version__ = '1.7.23'
 
 
 _ITERABLE_SINGLE_VALUES = dict, str, bytes
