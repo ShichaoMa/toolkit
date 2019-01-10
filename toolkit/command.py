3c66b9a626687f8b50df69ef3c20c833d373bcf5
马式超 <shichao.ma@yiducloud.cn>
2019-01-10 10:02:45 +0800

    Fix

--- toolkit/command.py
+++ toolkit/command.py
@@ -10,18 +10,16 @@ class MarkDownHelper(Service):
     def run(self):
         buffer = markdown(open(self.args.input).read(),
                           extensions=['markdown.extensions.extra'])
-        with open(self.args.output, "w") as f:
+        output = self.args.input + ".html"
+        with open(output, "w") as f:
             f.write(f"<html><body>{buffer}</body></html>")
 
-        self.logger.info(f"打开{self.args.output}.")
-        os.system(f"open {self.args.output}")
+        self.logger.info(f"打开{output}.")
+        os.system(f"open {output}")
 
     def enrich_parser_arguments(self):
         super(MarkDownHelper, self).enrich_parser_arguments()
-        self.parser.add_argument(
-            "-i", "--input", required=True, help="markdown file. ")
-        self.parser.add_argument(
-            "-o", "--output", help="html file", default="output.html")
+        self.parser.add_argument("input", help="markdown file.")
 
 
 def main():
