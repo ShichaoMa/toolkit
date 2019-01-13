e2a37c08815ad3dc803590905b86b0721770a36d
马式超 <shichao.ma@yiducloud.cn>
2019-01-13 18:30:56 +0800

    Feature: 增加css

--- toolkit/command.py
+++ toolkit/command.py
@@ -4,6 +4,31 @@ from markdown import markdown
 
 from .monitors import Service
 
+tpl = """
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="%s">
+<style>
+    .markdown-body {
+        box-sizing: border-box;
+        min-width: 200px;
+        max-width: 980px;
+        margin: 0 auto;
+        padding: 45px;
+    }
+ 
+    @media (max-width: 767px) {
+        .markdown-body {
+            padding: 15px;
+        }
+    }
+</style> 
+<article class="markdown-body">
+    %s
+</article>
+</html>
+"""
+
 
 class MarkDownHelper(Service):
 
@@ -11,8 +36,13 @@ class MarkDownHelper(Service):
         buffer = markdown(open(self.args.input).read(),
                           extensions=['markdown.extensions.extra'])
         output = self.args.input + ".html"
+        css = self.args.css
+
         with open(output, "w") as f:
-            f.write(f"<html><body>{buffer}</body></html>")
+            f.write(tpl % (css, buffer))
+
+        with open(os.path.join(os.path.dirname(output), css), "w") as f:
+            f.write(open(os.path.join(os.path.dirname(__file__), css)).read())
 
         self.logger.info(f"打开{output}.")
         os.system(f"open {output}")
@@ -20,6 +50,8 @@ class MarkDownHelper(Service):
     def enrich_parser_arguments(self):
         super(MarkDownHelper, self).enrich_parser_arguments()
         self.parser.add_argument("input", help="markdown file.")
+        self.parser.add_argument(
+            "-c", "--css", help="css file name.", default="github-markdown.css")
 
 
 def main():
