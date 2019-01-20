da81a2bcc49fcc5e86884c53b264f6d1612fa91d
cnaafhvk <cnaafhvk888@gmail.com>
2019-01-20 21:59:22 +0800

    Feature: V1.7.28

--- toolkit/__init__.py
+++ toolkit/__init__.py
@@ -19,7 +19,7 @@ from queue import Empty
 from future.utils import raise_from
 from functools import wraps, reduce
 
-__version__ = '1.7.27'
+__version__ = '1.7.28'
 
 
 def test_prepare(search_paths :typing.List[str]=None):
