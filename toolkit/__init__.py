7f15ea999086886390a2135ab3f58e4d68c218a9
马式超 <shichao.ma@yiducloud.cn>
2019-03-21 14:07:10 +0800

    Bugix: 修复awt无法处理future的情况

--- toolkit/__init__.py
+++ toolkit/__init__.py
@@ -18,7 +18,7 @@ from queue import Empty
 from future.utils import raise_from
 from functools import wraps, reduce
 
-__version__ = '1.7.39'
+__version__ = '1.7.40'
 
 
 def test_prepare(search_paths :typing.List[str]=None):
